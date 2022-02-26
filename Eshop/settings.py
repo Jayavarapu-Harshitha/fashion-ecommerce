b6c3adf26401fea88df35a356c3007597d9782e5
Jayavarapu Lakshmi Venkata Sri Harshitha <87439958+Jayavarapu-Harshitha@users.noreply.github.com>
2022-02-26 08:18:21 +0530

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY =config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=False
 
-ALLOWED_HOSTS = ['fffecommerce.herokuapp.com','127.0.1']
+ALLOWED_HOSTS = ['fashion-ecommerceklu.herokuapp.com','127.0.1']
 
 
 # Application definition
