486b3f9059adec6be2dc0332e42f3879a8f718d7
Jayavarapu Lakshmi Venkata Sri Harshitha <87439958+Jayavarapu-Harshitha@users.noreply.github.com>
2022-02-27 17:18:32 +0530

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/products')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
