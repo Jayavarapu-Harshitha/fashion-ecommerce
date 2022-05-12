0bd9e6c2e5099009b86673f01943486e11e6a20a
Jayavarapu-Harshitha <2000030388@kluniversity.in>
2022-05-12 16:40:26 +0530

    my uploads

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -24,7 +24,6 @@ SECRET_KEY ='^_g%33qd(g8bjc+*40&uh(ptgkb$&-*+0!i3$lu7xj1u166cbb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=True
 
-ALLOWED_HOSTS = []
 ALLOWED_HOSTS = [ 'kluecommerce.azurewebsites.net','fashion-ecommerceklu.herokuapp.com','127.0.1']
 
 
